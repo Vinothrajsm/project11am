63874380fc004066901951ea130dd05601d9b8a2
Vinothrajsm <raj.vinoth792@gmail.com>
2023-06-26 13:53:33 +0530

    taskstart

---
+++ src/org/git/GitProject.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class GitProject {
+	public void studentname() {
+		System.out.println("vinoth");
+		
+	}
+	public static void main(String[] args) {
+		GitProject gp= new GitProject();
+		gp.studentname();
+		
+	}
+
+}
